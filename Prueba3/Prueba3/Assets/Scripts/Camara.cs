720916d31e0f1ae914c1b6b1fcb23c0d608ea1b5
vpe0001 <vpe0001@alu.ubu.es>
2017-05-02 14:43:54 +0200

    Inicio: prueba de wheel collider

---
+++ Prueba3/Prueba3/Assets/Scripts/Camara.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camara : MonoBehaviour {
+	public GameObject cubo;
+	private Vector3 offset; //La distancia entre el cubo y la camara
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - cubo.transform.position;
+	}
+	
+	// LateUpdate is called once per frame despues de que se hayan calculado el resto de cosas
+	void LateUpdate () {
+		transform.position = cubo.transform.position + offset;
+	}
+}
---
+++ Prueba3/Prueba3/Assets/Scripts/MoverCubo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverCubo : MonoBehaviour {
+	public float fuerza;
+	private Rigidbody rb_cubo;
+	// Use this for initialization
+	void Start () {
+		rb_cubo = GetComponent <Rigidbody> ();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 movimiento = new Vector3 (fuerza, 0.0f, 0.0f);
+
+		rb_cubo.AddForce (movimiento);
+	
+	}
+}
